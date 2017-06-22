82cad6d0de5dff8a5dfd9efe18051f6fa192b0ea
Spencer Snyder <sasnyde2@gmail.com>
2017-06-21 23:15:38 -0400

    fixed webpack for sass loading and auto prefixing

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,34 @@
-var ExtractTextPlugin = require('extract-text-webpack-plugin');
+const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const autoprefixer = require('autoprefixer');
 
-const extractSass = new ExtractTextPlugin({
-    filename: "[name].css",
-    disable: process.env.NODE_ENV === "development"
-});
+
+
+const postcss = {
+  loader: 'postcss-loader',
+  options: {
+    plugins() { return [autoprefixer({ browsers: 'last 3 versions' })]; }
+  }
+};
+
+const styles = {
+  test: /\.(scss)$/,
+  use: ExtractTextPlugin.extract(['css-loader', postcss, 'sass-loader'])
+};
+
 
 module.exports = {
-    entry: [
-        `${__dirname}/css/main.scss`,
-    ],
-    output: {
-        path: `${__dirname}/css/`,
-        publicPath: '/',
-        filename: 'main.css',
-    },
-    module: {
-        rules: [{
-            test: /\.scss$/,
-            use: extractSass.extract({
-                use: [{
-                    loader: "css-loader"
-                }, {
-                    loader: "sass-loader"
-                }],
-                // use style-loader in development
-                fallback: "style-loader"
-            })
-        }]
-    },
-    plugins: [
-        extractSass
-    ]
+  entry: [
+    `${__dirname}/css/main.scss`,
+  ],
+  output: {
+    path: `${__dirname}/css/`,
+    filename: 'main.css',
+  },
+  module: {
+    rules: [styles]
+  },
+  plugins: [
+    new ExtractTextPlugin("main.css")
+  ]
 };
