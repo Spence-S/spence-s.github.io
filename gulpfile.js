19b5501ab85ff92ae01726e4b9a25252ba02d7ec
Spencer Snyder <sasnyde2@gmail.com>
2017-06-24 00:54:37 -0400

    more gulp workflow configured

--- gulpfile.js
+++ gulpfile.js
@@ -2,15 +2,42 @@ const gulp = require('gulp');
 const sass = require('gulp-sass');
 const autoprefixer = require('gulp-autoprefixer');
 const sourcemaps = require('gulp-sourcemaps');
-const uglify = require('gulp-uglify')
+const uglify = require('gulp-uglify');
+const clean = require('gulp-clean');
 
-gulp.task('css', function(){
+
+// CSS
+gulp.task('css', function() {
   return gulp.src('sass/main.scss')
     .pipe(sourcemaps.init())
-    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+    .pipe(sass().on('error', sass.logError)) //{outputStyle: 'compressed'}
     .pipe(autoprefixer({
-      browswers: ['last 3 versions']
+      browsers: ['last 3 versions']
     }))
     .pipe(sourcemaps.write('./maps'))
-    .pipe(gulp.dest('css/'))
+    .pipe(gulp.dest('assets/css/'))
+})
+
+// Fonts
+gulp.task('fonts', function() {
+  return gulp.src('node_modules/font-awesome/fonts/')
+    .pipe(gulp.dest('assets/fonts/'))
+})
+
+// Clean
+
+gulp.task('clean', function() {
+  return gulp.src('assets/')
+    .pipe(clean())
+})
+
+// watcher
+gulp.task('watch', [ 'css' ], (done) => {
+  gulp.watch('sass/*.scss', [ 'css' ]);
+});
+
+
+const watcher = gulp.watch('sass/*.scss',['css']);
+watcher.on('change', function(event) {
+  console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
 })
